e9f466c85087e2ed4c0c04707b416058cf629405
l-f1d <f1d.pro.luca@gmail.com>
2020-10-18 00:41:15 +0100

    type hinting

---
+++ Section 8 - Type hinting in Python/scratch.py
@@ -0,0 +1,26 @@
+from typing import List, Dict, Union
+
+def my_function() -> None: # -> None *type hint*
+    pass
+
+
+hoover_over_me = my_function()
+
+def another_func() -> List[Dict(str, Union(str, int))]: # List of Dict(s) each of which has KEYS of type 'str' and values of type "str" and/or "int"
+    return [{"a": "a", "b": "b", "c": 0}, {"a": "a", "b": "b", "c": 0}]
+
+
+data = another_func()
+print(data)
+
+
+"""
+Our own Types
+"""
+Book = Dict(str, Union(str, int))
+def again_a_fn(a: str, b: str) -> List[Book]: # type hint for parameters
+    return [{"a": a, "b": b, "c": 0}, {"a": "a", "b": "b", "c": 0}]
+
+
+more_data = again_a_fn("a", "b")
+print(more_data)
