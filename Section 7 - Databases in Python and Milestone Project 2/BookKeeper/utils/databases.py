53ea8cfd257ebe2d060f6ec09a3fb8959da134da
l-f1d <f1d.pro.luca@gmail.com>
2020-10-18 00:05:51 +0100

    CONTEXT MANAGERgit status!

--- Section 7 - Databases in Python and Milestone Project 2/BookKeeper/utils/databases.py
+++ Section 7 - Databases in Python and Milestone Project 2/BookKeeper/utils/databases.py
@@ -1,51 +1,38 @@
-import sqlite3
+from .database_connection import DatabaseConnection
+
+db_file = "data.db"
 
 def create_book_table():
-    db = sqlite3.connect("data.db")
-    cursor = db.cursor()
+    with DatabaseConnection(db_file) as db:
+        cursor = db.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)")
-
-    db.commit()
-    db.close()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)")
 
 
 def list_all_books():
-    db = sqlite3.connect("data.db")
-    cursor = db.cursor()
+    with DatabaseConnection(db_file) as db:
+        cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM books") # get everything from the table
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] # read table line by line and convert to dict
-
-    db.close()
-    return books
+        cursor.execute("SELECT * FROM books") # get everything from the table
+        return [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()] # read table line by line and convert to dict
 
 
 def add_book(name, author):
-    db = sqlite3.connect("data.db")
-    cursor = db.cursor()
+    with DatabaseConnection(db_file) as db:
+        cursor = db.cursor()
 
-    cursor.execute("INSERT INTO books VALUES(?, ?, 0)", (name, author)) # do this to prevent SQL injection
-
-    db.commit()
-    db.close()
+        cursor.execute("INSERT INTO books VALUES(?, ?, 0)", (name, author)) # do this to prevent SQL injection
 
 
 def mark_as_read(name):
-    db = sqlite3.connect("data.db")
-    cursor = db.cursor()
+    with DatabaseConnection(db_file) as db:
+        cursor = db.cursor()
 
-    cursor.execute("UPDATE books SET read=1 WHERE name=?", (name,))
-
-    db.commit()
-    db.close()
+        cursor.execute("UPDATE books SET read=1 WHERE name=?", (name,))
 
 
 def delete_book(name):
-    db = sqlite3.connect("data.db")
-    cursor = db.cursor()
+    with DatabaseConnection(db_file) as db:
+        cursor = db.cursor()
 
-    cursor.execute("DELETE FROM books WHERE name=?", (name,))
-
-    db.commit()
-    db.close()
+        cursor.execute("DELETE FROM books WHERE name=?", (name,))
