98153fa043a191d9a7d92a5f9b524561ede492ac
l-f1d <f1d.pro.luca@gmail.com>
2020-10-20 13:06:22 +0100

    wait in selenium

--- Section 12: Browser Automation with Selenium/MP3: Quote Scraper/pages/quotes_page.py
+++ Section 12: Browser Automation with Selenium/MP3: Quote Scraper/pages/quotes_page.py
@@ -1,6 +1,10 @@
 from typing import List
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+
 
 from locators.quotes_page_locators import QuotesPageLocators
 from parsers.quote import QuoteParser
@@ -49,6 +53,13 @@ class QuotesPage:
 
     def search_for_quotes(self, author_name: str, tag_name: str) -> List[QuoteParser]:
         self.select_author(author_name)
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.CSS_SELECTOR, QuotesPageLocators.TAG_DROPDOWN_VALUE_OPTION)
+            )
+        )
+
         try:
             self.select_tag(tag_name)
         except NoSuchElementException:
