ab696036345e65ddb544eadcce058ff2f1aedf63
l-f1d <f1d.pro.luca@gmail.com>
2020-10-20 12:51:03 +0100

    selenium

--- Section 12: Browser Automation with Selenium/MP3: Quote Scraper/app.py
+++ Section 12: Browser Automation with Selenium/MP3: Quote Scraper/app.py
@@ -1,5 +1,5 @@
 """
-Milestone Project 3: A Quote Scraper
+MP3: Rewrite A Quote Scraper using Selenium instead of bs4
 """
 try:
     from selenium import webdriver
@@ -9,11 +9,20 @@ except:
     from selenium import webdriver
 
 
-from pages.quotes_page import QuotesPage
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError
 
-chrome = webdriver.Chrome(executable_path="/home/luca/chromedriver")
-chrome.get("http://quotes.toscrape.com")
-page = QuotesPage(chrome)
+try:
+    author = input("Enter the name of the author: ")
+    tag = input("Enter tag: ")
 
-for quote in page.quotes:
-    print(quote)
+    chrome = webdriver.Chrome(executable_path="/home/luca/chromedriver")
+    chrome.get("http://quotes.toscrape.com/search.aspx")
+    page = QuotesPage(chrome)
+
+    print(page.search_for_quotes(author, tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error occurred. Please try again.")
+    
