f3df9472d94c51088a34591a0e464640a9b7e1bf
l-f1d <f1d.pro.luca@gmail.com>
2020-10-21 01:30:01 +0100

    migrate to aiohttp

--- Section 11: Web Scraping with Python/MP4: BookScraper/app.py
+++ Section 11: Web Scraping with Python/MP4: BookScraper/app.py
@@ -1,3 +1,4 @@
+import asyncio, aiohttp,async_timeout, time
 import requests
 import logging
 
@@ -6,7 +7,7 @@ from pages.all_books_page import AllBooksPage
 logging.basicConfig(
     format='%(ascitime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
-    level=logging.DEBUG,
+    level=logging.CRITICAL,
     filename='logs.txt'
 )
 logger = logging.getLogger('scraping')
@@ -19,12 +20,35 @@ content = response.content
 
 page = AllBooksPage(content)
 
+loop = asyncio.get_event_loop()
+
 books = page.books
 
-for page_num in range(1, page.page_count):
-    url = f"http://books.toscrape.com/catalogue/page-{page_num+1}.html"
-    page_content = requests.get(url).content
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f"Page took {time.time() - page_start}s to load")
+            return await response.text()
+
+
+async def get_multiple_pages(loop, urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+urls = [f"http://books.toscrape.com/catalogue/page-{page_num+1}.html" for page_num in range(1, page.page_count)]
+start = time.time()
+pages = loop.run_until_complete(get_multiple_pages(loop, urls))
+print(f"Total pages requests took {time.time() - start}s")
+
+for page_content in pages:
     logger.debug("Creating AllBooksPage from page content.")
     page = AllBooksPage(page_content)
     books.extend(page.books)
+
 
